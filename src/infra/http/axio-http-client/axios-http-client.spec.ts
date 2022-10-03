58ac0344549e648e9c0a877d0dd13a3fce0490b6
raulrocha2 <raul.rocha2@gmail.com>
2022-10-03 19:04:48 -0300

    test: ensure AxiosHttpClient calls axios with correct values

--- src/infra/http/axio-http-client/axios-http-client.spec.ts
+++ src/infra/http/axio-http-client/axios-http-client.spec.ts
@@ -16,11 +16,11 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 })
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL and verb', async () => {
-    const { url } = mockPostRequest()
-    const sut = makeSut()
-    await sut.post({ url })
-    expect(mockedAxios.post).toHaveBeenCalledWith(url)
-  })
 
+  test('Should call axios with correct values', async () => {
+    const sut = makeSut()
+    const request = mockPostRequest()
+    await sut.post(request)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
 })
