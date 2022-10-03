84bb6d2dabfe045dea5d99be26fd685b2a19c233
raulrocha2 <raul.rocha2@gmail.com>
2022-10-03 13:29:37 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,8 +1,6 @@
-import { AuthenticationParams, IAuthentication } from "@/domain/usecases/iauthentication";
-import { IHttpPostClient } from "@/data/protocols/http/http-post-client";
-import { HttpStatusCode } from "@/data/protocols/http/http-response";
-import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
-import { UnexpectedError } from "@/domain/errors/unexpected-error";
+import { AuthenticationParams, IAuthentication } from "@/domain/usecases";
+import { IHttpPostClient, HttpStatusCode } from "@/data/protocols/http";
+import { InvalidCredentialsError, UnexpectedError } from "@/domain/errors";
 import { AccountModel } from "@/domain/models/account-model";
 
 export class RemoteAuthentication implements IAuthentication {
