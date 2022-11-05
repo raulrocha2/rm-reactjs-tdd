6e9abf938b3ebc07bb8d1006a20d85823fe0375a
raulrocha2 <raul.rocha2@gmail.com>
2022-11-05 20:59:55 -0300

    refactor: move ValidatioSpy to one file

---
+++ src/presentation/test/mock-validation.ts
@@ -0,0 +1,13 @@
+import { IValidation } from '../protocols/validaion'
+
+export class ValidationSpy implements IValidation {
+  errorMessage: string
+  fieldName: string
+  fieldValue: string
+
+  validate (fieldName: string, fieldValue: string): string {
+    this.fieldName = fieldName
+    this.fieldValue = fieldValue
+    return this.errorMessage
+  }
+}
