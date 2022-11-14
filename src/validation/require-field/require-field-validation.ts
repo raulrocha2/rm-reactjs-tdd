4e5a9642a6f81f3d2ac5fdce8b45a210ff82f439
raulrocha2 <raul.rocha2@gmail.com>
2022-11-14 17:14:48 -0300

    refactor: move folder protocols do root validation

--- src/validation/require-field/require-field-validation.ts
+++ src/validation/require-field/require-field-validation.ts
@@ -1,5 +1,5 @@
 import { RequiredFieldError } from '@/validation/errors'
-import { IFieldValidation } from './protocols/i-field-validation'
+import { IFieldValidation } from '../protocols/i-field-validation'
 
 export class RequiredFieldValidation implements IFieldValidation {
   constructor (readonly field: string) { }
