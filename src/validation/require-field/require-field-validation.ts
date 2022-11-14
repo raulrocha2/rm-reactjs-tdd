776ba16f3fd6a971b3c5bcc1a8065f816cba9d12
raulrocha2 <raul.rocha2@gmail.com>
2022-11-14 17:13:13 -0300

    feat: ensure RequiredFieldValidation return falsy if field is not empty

--- src/validation/require-field/require-field-validation.ts
+++ src/validation/require-field/require-field-validation.ts
@@ -4,6 +4,6 @@ import { IFieldValidation } from './protocols/i-field-validation'
 export class RequiredFieldValidation implements IFieldValidation {
   constructor (readonly field: string) { }
   validate (value: string): Error {
-    return new RequiredFieldError()
+    return value ? null : new RequiredFieldError()
   }
 }
