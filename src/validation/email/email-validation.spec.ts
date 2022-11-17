298c3ee10c05c6a50909514dfb5037cc27dce7a8
raulrocha2 <raul.rocha2@gmail.com>
2022-11-17 11:19:41 -0300

    refactor: add faker EmailValidation test

--- src/validation/email/email-validation.spec.ts
+++ src/validation/email/email-validation.spec.ts
@@ -1,16 +1,17 @@
+import { faker } from '@faker-js/faker'
 import { InvalidFieldError } from '../errors'
 import { EmailValidation } from './email-validation'
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
     const sut = new EmailValidation('email')
-    const error = sut.validate('invalid-email.com')
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return null if email is valid', () => {
     const sut = new EmailValidation('email')
-    const error = sut.validate('valid@email.com')
+    const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
 })
