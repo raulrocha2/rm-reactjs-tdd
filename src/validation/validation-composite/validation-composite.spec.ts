0a9fe8e5cb0ffd8c775d739d207cb9b6f8fc9364
raulrocha2 <raul.rocha2@gmail.com>
2022-11-17 14:58:24 -0300

    test: ensure ValidationComposite return the first erro if any fails

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,16 +1,23 @@
+import { IFieldValidation } from '../protocols/i-field-validation'
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
+
+const makeSut = (validators: IFieldValidation[]): ValidationComposite => {
+  const sut = new ValidationComposite(validators)
+  return sut
+}
 
 describe('ValidationComposite', () => {
   test('Should return error if any validations fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_error_message')
-    const sut = new ValidationComposite([
+    fieldValidationSpy.error = new Error('first_error_message')
+    fieldValidationSpy2.error = new Error('second_error_message')
+    const sut = makeSut([
       fieldValidationSpy,
       fieldValidationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_error_message')
+    expect(error).toBe('first_error_message')
   })
 })
