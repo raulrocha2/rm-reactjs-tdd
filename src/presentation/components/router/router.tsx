97995ac62b5eccf22a1f2d2223299e6a7c47144b
raulrocha2 <raul.rocha2@gmail.com>
2022-11-19 17:16:04 -0300

    feat: add login composition

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Login } from '@/presentation/pages'
 import {
   BrowserRouter,
   Switch,
   Route
 } from 'react-router-dom'
 
-const Router: React.FC = () => {
+type Props = {
+  makeLogin: React.FC
+}
+
+const Router: React.FC<Props> = ({ makeLogin }: Props) => {
   return (
    <BrowserRouter>
     <Switch>
-      <Route path="/login" component={Login} />
+      <Route path="/login" component={makeLogin} />
     </Switch>
    </BrowserRouter>
   )
