65aaf4a39175079f01e0c88dbf254111c317c589
raulrocha2 <raul.rocha2@gmail.com>
2022-11-27 19:23:04 -0300

    fix: change webpack env var to use JSON.stringify

---
+++ src/main/factories/cache/local-storage-adapter-factory.ts
@@ -0,0 +1,6 @@
+import { ISetStorage } from "@/data/protocols/http/cache/i-set-storage"
+import { LocalStorageAdapter } from "@/infra/cache/local-storage-adapter"
+
+export const makeLocalStorageAdapter = (): ISetStorage => {
+  return new LocalStorageAdapter()
+}
