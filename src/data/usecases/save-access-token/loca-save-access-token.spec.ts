e466ff514a3b27edb8ba3b3e9ca24d2db10ea972
raulrocha2 <raul.rocha2@gmail.com>
2022-11-26 21:54:19 -0300

    refactor: move create LocalSaveAccessToken to makeSut factory

--- src/data/usecases/save-access-token/loca-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/loca-save-access-token.spec.ts
@@ -1,12 +1,25 @@
 import { SetStorageSpy } from "@/data/test"
 import { LocalSaveAccessToken } from "./loca-save-access-token"
 import { faker } from "@faker-js/faker"
+import { ISetStorage } from "@/data/protocols/http/cache/i-set-storage"
 
+type SutTypes = {
+  setStorageSpy: SetStorageSpy
+  sut: LocalSaveAccessToken
+}
+
+const makeSut = (): SutTypes => {
+  const setStorageSpy = new SetStorageSpy()
+  const sut = new LocalSaveAccessToken(setStorageSpy)
+  return {
+    sut,
+    setStorageSpy
+  }
+}
 
 describe('LocalSaveAccessToken', () => {
   test('Should call SetStorage with correc value', async () => {
-    const setStorageSpy = new SetStorageSpy()
-    const sut = new LocalSaveAccessToken(setStorageSpy)
+    const { sut, setStorageSpy } = makeSut()
     const accessToken = faker.random.alphaNumeric(5)
     await sut.save(accessToken)
     expect(setStorageSpy.key).toBe('accessToken')
