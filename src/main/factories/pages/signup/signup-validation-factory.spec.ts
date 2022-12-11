17cb1a221808323ef44e40ce61d89327ba1524f6
raulrocha2 <raul.rocha2@gmail.com>
2022-12-11 20:17:38 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+
+import { ValidationBuilder as Builder } from '@/validation/builder/validation-builder'
+import { ValidationComposite } from '@/validation/validation-composite/validation-composite'
+import { makeSignUpValidation } from './signup-validation-factory'
+
+describe('SignValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).build()
+    ]))
+  })
+})
