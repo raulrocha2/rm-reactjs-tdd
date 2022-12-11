a1a2bebed2e91a7c3fe498be794a301b82fc56aa
raulrocha2 <raul.rocha2@gmail.com>
2022-12-11 17:18:14 -0300

    test: ensure SignUp present error if SaveAccessToken fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -173,4 +173,13 @@ describe('Signup Component', () => {
     expect(history.length).toBe(1)
     expect(history.location.pathname).toBe('/')
   })
+
+  test('Should present error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut()
+    const error = new EmailInUseError()
+    jest.spyOn(saveAccessTokenMock, 'save').mockRejectedValueOnce(error)
+    await simulateValidSubmit(sut)
+    await waitFor(() => Helper.testElementText(sut, 'main-error', error.message))
+    Helper.testChildCount(sut, 'error-wrap', 1)
+  })
 })
